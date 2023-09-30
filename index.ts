413da7067602bbf3f315d80bcaadeccf8b9a4322
ahmet <prtkgmr@gmail.com>
2023-09-30 18:22:48 +0300

    push

--- index.ts
+++ index.ts
@@ -1,10 +1,39 @@
-import figlet from  "figlet";
-console.log("Hello via Bun!");
-const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-        const body = figlet.textSync("Bun!");
-        return new Response(body);
-    },
+// index.ts
+import { Bun, Response } from "bun";
+import figlet from "figlet";
+
+const app = new Bun();
+
+app.route("/", (req) => {
+    const body = figlet.textSync("Welcome to my website!");
+    return new Response(
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>My Website</title>
+                <style>
+                    body { 
+                        font-family: monospace 
+                        display: flex; 
+                        justify-content: center; 
+                        align-items: center; 
+                        height: 100vh; 
+                        margin: 0; 
+                        background-color: #282c34; 
+                        color: #61dafb; 
+                    }
+                </style>
+            </head>
+            <body>
+                <pre>${body}</pre>
+            </body>
+        </html>
+        `,
+        { headers: { "Content-Type": "text/html" } }
+    );
 });
-console.log(`Listening on http://localhost:${server.port} ...`);
+
+app.listen({ port: 3000 });
+console.log(Bun.version);
+console.log("Server running on http://localhost:3000");
